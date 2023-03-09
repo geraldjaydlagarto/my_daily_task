90da74af7ae2cea61fe719027f1e4970f50bd67b
Gerald Jay Lagarto <glagarto@startechup.com>
2023-03-10 01:42:01 +0800

    Design the daily task item for content.

--- app/src/main/java/co/geraldjaydlagarto/mydailytask/ui/list/TaskListScreen.kt
+++ app/src/main/java/co/geraldjaydlagarto/mydailytask/ui/list/TaskListScreen.kt
@@ -1,16 +1,24 @@
 package co.geraldjaydlagarto.mydailytask.ui.list
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.geraldjaydlagarto.mydailytask.R
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
+@Preview
 fun TaskListScreen() {
     Scaffold(
         topBar = {
@@ -24,6 +32,7 @@ fun TaskListScreen() {
 }
 
 @Composable
+@Preview
 private fun TopBar(modifier: Modifier = Modifier) {
     SmallTopAppBar(
         title = {
@@ -39,6 +48,51 @@ private fun TopBar(modifier: Modifier = Modifier) {
 
 @Composable
 private fun Content(modifier: Modifier = Modifier) {
-    
+    LazyColumn(
+        modifier = modifier
+            .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+            .fillMaxSize()
+    ) {
+        item {
+            DailyTaskItem()
+        }
+    }
 }
+
+@Composable
+@Preview
+private fun DailyTaskItem() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Yellow)
+            .absolutePadding(10.dp)
+            .heightIn(54.dp)
+            .clickable {
+
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.titleMedium,
+            text = "Design the app",
+        )
+        Text(
+            text = "120",
+            style = MaterialTheme.typography.labelMedium
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_schedule_24),
+            contentDescription = "Duration"
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_chevron_right_24),
+            contentDescription = null
+        )
+    }
+}
+
+
 
